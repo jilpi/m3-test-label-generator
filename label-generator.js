8d460d835bf5e948af2052ab5ceb816b514307a8
Jean-Laurent Picard <jl@pi.cards>
2020-11-08 20:02:22 +0100

    fields can be changed by URL params

---
+++ label-generator.js
@@ -0,0 +1,27 @@
+// URL example: label-generator.html?booking=201108-ABCDEFG&site=Chêne&service-type=COV19-RAPID&firstname=John&lastname=Doe&sex=M&dob=01-JAN-2000&avs=0123456789&tel=+41.12345678&email=john.doe@m-3sanitrade.ch
+
+const queryString = window.location.search;
+console.log(queryString);
+
+const urlParams = new URLSearchParams(queryString);
+
+
+const paramlist = [
+    "booking",
+    "site",
+    "service-type",
+    "firstname",
+    "lastname",
+    "dob",
+    "sex",
+    "avs",
+    "tel",
+    "email"
+];
+
+paramlist.forEach ((param)=>{
+    if (urlParams.has(param))
+        document.getElementsByTagName(`lbl-${param}`)[0].innerHTML=urlParams.get(param);
+})
+
+document.getElementById('qrcode').innerText = urlParams.get('booking');
